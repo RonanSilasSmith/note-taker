0b0c340b803ecec73cfdf04524d2e893c491b777
RonanSilasSmith <smith.ronan@outlook.com>
2020-12-28 01:14:21 -0900

    adds find by id routes

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ const PORT = process.env.PORT || 3001;
 //gets what we need out of the way^
 
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(notes => notes.id === id)[0];
+    return result;
+}
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    res.json(result);
+});
 
 //goes last
 app.listen(3001, () => {
